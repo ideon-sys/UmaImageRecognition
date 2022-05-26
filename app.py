7f9e45db91d8118738bed8583da4a5ed4927ba59
ideon-sys <sesinem@gmail.com>
2022-05-27 01:50:08 +0900

    specify utf-8 encoding commit

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import os
 import sys
+import datetime
 import uma_detail
 
 
+dt_now = datetime.datetime.now()
+
 if len(sys.argv) <= 1:
     print("第一引数に読み込みたいファイルを指定して下さい")
-    exit()
-if not os.path.exists(sys.argv[1]):
+    msg = "ERROR [{0}] : 第一引数に読み込みたいファイルを指定して下さい。\n".format(dt_now)
+elif not os.path.exists(sys.argv[1]):
     print("存在するファイルを指定して下さい。")
-    exit()
+    msg = "ERROR [{0}] : 存在するファイルを指定して下さい。\n".format(dt_now)
+else:
+    try:
+        src = uma_detail.imread_umadetail(sys.argv[1])
+        txt = uma_detail.get_status_to_json(src)
+        print("結果")
+        print(txt)
+        msg = "INFO [{0}] : {1}\n".format(dt_now, txt)
+    except IndexError as e:
+        print("ファイルの読込に失敗しました。PNG形式のウマ娘詳細画面を使用して下さい。")
+        msg = "ERROR [{0}] : ファイルの読込に失敗しました。PNG形式のウマ娘詳細画面を使用して下さい。\n".format(dt_now)
 
-try:
-    src = uma_detail.imread_umadetail(sys.argv[1])
-    txt = uma_detail.get_status_to_json(src)
-except IndexError as e:
-    print("ファイルの読込に失敗しました。PNG形式のウマ娘詳細画面を使用して下さい。")
-    exit()
-print("結果")
-print(txt)
+with open("result.txt", mode='a', encoding='utf-8') as fstream:
+    fstream.write(msg)
